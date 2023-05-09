0a43ec28b5a47d5ffc8f363dc8ed23259b64c878
gamdwk <728882065@qq.com>
2023-05-09 21:06:15 +0800

    init

---
+++ partition.sh
@@ -0,0 +1,2 @@
+cd /home/sj/nvme/DGC
+python3 partition_graph.py --dataset ogb-product --num_parts 2
---
+++ run.sh
@@ -0,0 +1,10 @@
+cd /home/sj/nvme/DGC
+python3 /home/sj/nvme/DGC/launch.py \
+--workspace /home/sj/nvme/DGC \
+--num_trainers 1 \
+--num_samplers 1 \
+--num_servers 1 \
+--extra_envs PYTHONPATH=/home/sj/sdb/anaconda3/envs/dgs/bin/python3:$PYTHONPATH \
+--part_config data/ogb-product.json \
+--ip_config ip_config.txt \
+"/home/sj/sdb/anaconda3/envs/dgs/bin/python3 train_dist.py --graph_name ogb-product --ip_config ip_config.txt --num_epochs 30 --batch_size 1024 --num_gpus=2"
